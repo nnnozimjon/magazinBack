789e8ce9d19a19b1856e1a5e090bbd6411d1cdfb
nnnozimjon <iamnozimjon@gmail.com>
2023-08-24 19:09:13 +0500

    +

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,7 @@ import Api from '../constants'
 const Router = express.Router()
 
 Router.get('/', (req: Request, res: Response) => {
-<<<<<<< HEAD
-  res.send('Development!')
-=======
   res.send('Production!')
->>>>>>> 536bbf2 (added index route)
 })
 
 export default Router
