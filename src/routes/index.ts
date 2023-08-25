3d7e276a70c69ab6d4c7858a62fb32fc16643f2e
nnnozimjon <iamnozimjon@gmail.com>
2023-08-25 10:37:45 +0500

    feat: swagger created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
 import express, { Response, Request } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocumentOne from '../docs/swagger.json'
 // import System from '../controllers'
 // import Api from '../constants'
 
 const Router = express.Router()
+
+Router.use(
+  '/api-docs',
+  swaggerUi.serveFiles(swaggerDocumentOne),
+  swaggerUi.setup(swaggerDocumentOne, { explorer: true })
+)
 
 Router.get('/', (req: Request, res: Response) => {
   res.send('Production!')
