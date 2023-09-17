eb9145ce1f503af8acdb971ce2c3705ac9ce606a
nnnozimjon <iamnozimjon@gmail.com>
2023-09-17 16:36:30 +0500

    +

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 // const baseURL: string = 'http://localhost:8002'
-const baseURL: string = 'https://dev-api.dushanbemarket.com'
+const baseURL: string = 'http://dev-api.dushanbemarket.com'
 
 export default baseURL
